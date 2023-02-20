d6d54068bb409d47fb60cedf84632025d6280126
Rafał Olejniczak <olejniczak00@gmail.com>
2023-02-20 01:12:24 +0100

    New Fragment

---
+++ app/src/main/java/com/RafalEngiWork/MLTraders/StatisticsFragment.kt
@@ -0,0 +1,35 @@
+package com.RafalEngiWork.MLTraders
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.RafalEngiWork.MLTraders.databinding.FragmentStatisticsBinding
+
+class StatisticsFragment : Fragment() {
+
+    private var _binding: FragmentStatisticsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
