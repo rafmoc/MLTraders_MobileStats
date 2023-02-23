7abb88927911255bc74a2b4ef8903e5917b5833f
Rafał Olejniczak <olejniczak00@gmail.com>
2023-02-24 00:06:19 +0100

    Statistics finished

--- app/src/main/java/com/RafalEngiWork/MLTraders/StatisticsFragment.kt
+++ app/src/main/java/com/RafalEngiWork/MLTraders/StatisticsFragment.kt
@@ -20,15 +20,17 @@ class StatisticsFragment : Fragment() {
 
     lateinit var lineGraphView: GraphView
     lateinit var lineGraphView2: GraphView
+    private var logNumber: Int = 0
+    private var maxLogNumber: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -37,22 +39,42 @@ class StatisticsFragment : Fragment() {
             ml.name == nameOfData
         }
 
-        setGeneralInformations(nameOfData, mLRuns)
-        setEarningsAndLosesChart(mLRuns)
+        if(mLRuns != null)
+        {
+            maxLogNumber = (mLRuns?.mLRuns?.size?.minus(1))?:0
+            logNumber = maxLogNumber
+            binding.currentLogNumber.text = "Log nr: ${logNumber}"
+
+            binding.logMinus.setOnClickListener {
+                logNumber = logNumber.dec()
+                if(logNumber < 0) { logNumber = 0 }
+                binding.currentLogNumber.text = "Log nr: ${logNumber}"
+                setGeneralInformations(nameOfData, mLRuns)
+            }
+            binding.logPlus.setOnClickListener {
+                logNumber = logNumber.inc()
+                if(logNumber > maxLogNumber) { logNumber = maxLogNumber }
+                binding.currentLogNumber.text = "Log nr: ${logNumber}"
+                setGeneralInformations(nameOfData, mLRuns)
+            }
+
+            setGeneralInformations(nameOfData, mLRuns)
+            setEarningsAndLosesChart(mLRuns)
+        }
     }
 
     @SuppressLint("SetTextI18n")
     private fun setGeneralInformations(
         nameOfData: String?, mLRuns: MLRuns?
     ) {
-        mLRuns?.mLRuns?.last()?.let { lastRun ->
+        mLRuns?.mLRuns!![logNumber].let { runNumber ->
             binding.mLRunTopText.text = nameOfData
-            binding.statsView1Value.text = "${lastRun.steps}"
-            binding.statsView2Value.text = "${lastRun.earnedCredits + lastRun.lostCredits + lastRun.sameCredits}"
-            binding.statsView3Value.text = "${lastRun.creditBalance}"
-            binding.statsView4Value.text = "${lastRun.earnedCredits}"
-            binding.statsView5Value.text = "${lastRun.lostCredits}"
-            binding.statsView6Value.text = "${lastRun.sameCredits}"
+            binding.statsView1Value.text = "${runNumber.steps}"
+            binding.statsView2Value.text = "${runNumber.earnedCredits + runNumber.lostCredits + runNumber.sameCredits}"
+            binding.statsView3Value.text = "${runNumber.creditBalance}"
+            binding.statsView4Value.text = "${runNumber.earnedCredits}"
+            binding.statsView5Value.text = "${runNumber.lostCredits}"
+            binding.statsView6Value.text = "${runNumber.sameCredits}"
         }
     }
 
@@ -73,25 +95,12 @@ class StatisticsFragment : Fragment() {
         }?.toTypedArray()
 
         val creditsBalancePoints: Array<DataPoint>? = mLRuns?.mLRuns?.mapIndexed { index, mlRun ->
-            intToDataPoint((index + 1), normalizeToHundred(mlRun.creditBalance,
-                mLRuns.mLRuns.minOf { it.creditBalance },
-                mLRuns.mLRuns.maxOf { it.creditBalance }).toInt()
+            intToDataPoint(
+                (index + 1), normalizeToHundred(mlRun.creditBalance,
+                    mLRuns.mLRuns.minOf { it.creditBalance },
+                    mLRuns.mLRuns.maxOf { it.creditBalance }).toInt()
             )
         }?.toTypedArray()
-
-        val creditsBalancePointsTEST: List<Int>? = mLRuns?.mLRuns?.mapIndexed { index, mlRun ->
-            intToDataPoint((index + 1), normalizeToHundred(mlRun.creditBalance,
-                mLRuns.mLRuns.minOf { it.creditBalance },
-                mLRuns.mLRuns.maxOf { it.creditBalance }).toInt()
-            )
-            Log.d("FireLog", normalizeToHundred(mlRun.creditBalance,
-                mLRuns.mLRuns.minOf { it.creditBalance },
-                mLRuns.mLRuns.maxOf { it.creditBalance }).toString()
-            )
-            Log.d("FireLog", mlRun.creditBalance.toString())
-            Log.d("FireLog", mLRuns.mLRuns.minOf { it.creditBalance }.toString())
-            Log.d("FireLog", mLRuns.mLRuns.maxOf { it.creditBalance }.toString())
-        }
 
         val earnSeries: LineGraphSeries<DataPoint> = LineGraphSeries(earnedPoints)
         earnSeries.color = resources.getColor(R.color.primary_700)
@@ -126,7 +135,7 @@ class StatisticsFragment : Fragment() {
     }
 
     private fun normalizeToHundred(value: Int, min: Int, max: Int): Float {
-        if(max < 0)
+        if (max < 0)
             return (((value - min) / (max - min).toFloat()) * 100) - 150
         return (((value - min) / (max - min).toFloat()) * 100) - 50
     }
